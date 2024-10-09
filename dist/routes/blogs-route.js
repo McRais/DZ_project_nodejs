e9c615dbb611001915f69c245e702ebc37a97a33
McRais <mcrais21@gmail.com>
2024-10-09 16:07:28 +0300

    Update blogs-route.js

--- dist/routes/blogs-route.js
+++ dist/routes/blogs-route.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.blogsRoute = void 0;
 const express_1 = require("express");
@@ -7,20 +16,20 @@ const blogs_repository_1 = require("../repo/blogs-repository");
 const validator_blogs_1 = require("../validators/validator-blogs");
 exports.blogsRoute = (0, express_1.Router)({});
 //get all blogs
-exports.blogsRoute.get('/', (req, res) => {
-    const blog = blogs_repository_1.blogsRepo.getAllBlogs();
+exports.blogsRoute.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const blog = yield blogs_repository_1.blogsRepo.getAllBlogs();
     return res.send(blog);
-}); //done
+})); //done
 //get blog by id
-exports.blogsRoute.get('/:id', (req, res) => {
-    const blog = blogs_repository_1.blogsRepo.getBlogById(req.params.id);
+exports.blogsRoute.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const blog = yield blogs_repository_1.blogsRepo.getBlogById(req.params.id);
     if (blog === false) {
         return res.sendStatus(404);
     }
     else {
         return res.send(blog);
     }
-}); //done
+})); //done
 //delete blog by id, auth
 exports.blogsRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) => {
     const blog = blogs_repository_1.blogsRepo.deleteBlog(req.params.id);
