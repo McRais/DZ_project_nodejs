220981de12d418f6ef2fa0b0c503c16060806f65
McRais <mcrais21@gmail.com>
2024-10-17 19:09:47 +0300

    updated dist

--- dist/routes/blogs-route.js
+++ dist/routes/blogs-route.js
@@ -41,7 +41,7 @@ exports.blogsRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) =
     }
 })); //done
 //post blog, auth and validation
-exports.blogsRoute.post("/", auth_middleware_1.authMiddleware, (0, validator_blogs_1.blogValidation)(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.blogsRoute.post("/", auth_middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const blogId = yield blogs_repository_1.blogsRepo.createNewBlog(req.body.name, req.body.description, req.body.websiteUrl);
     const blog = yield blogs_repository_1.blogsRepo.getBlogById(blogId);
     return res.status(201).send(blog);
--- dist/validators/validator-blogs.js
+++ dist/validators/validator-blogs.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogValidation = exports.blogIdValidator = void 0;
+exports.blogValidation = void 0;
 const express_validator_1 = require("express-validator");
-const blogs_repository_1 = require("../repo/blogs-repository");
-const validator_errors_catcher_1 = require("../middlewares/validator-errors-catcher");
 const nameValidator = (0, express_validator_1.body)('name')
     .isString().withMessage('name must be a string')
     .trim()
@@ -11,26 +9,28 @@ const nameValidator = (0, express_validator_1.body)('name')
     min: 1,
     max: 15
 }).withMessage('incorrect name');
-const descriptionValidator = (0, express_validator_1.body)('description')
+/*const descriptionValidator = body('description')
     .isString().withMessage('description must be a string')
     .trim()
     .isLength({
-    min: 0,
+    min:0,
     max: 500
-}).withMessage('incorrect description');
-const websiteValidator = (0, express_validator_1.body)('websiteUrl')
+    }).withMessage('incorrect description')
+
+const websiteValidator = body('websiteUrl')
     .isString().withMessage('website URL must be a string')
     .trim()
     .isLength({
-    min: 0,
+    min:0,
     max: 100
-}).matches('^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$').withMessage('incorrect website URL');
-exports.blogIdValidator = (0, express_validator_1.body)('id')
-    .custom((id) => {
-    const blog = blogs_repository_1.blogsRepo.getBlogById(id);
-    if (blog === false) {
-        throw Error('incorrect id');
-    }
-});
-const blogValidation = () => [nameValidator, descriptionValidator, websiteValidator, validator_errors_catcher_1.validatorErrorsCatcher];
+    }).matches('^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$').withMessage('incorrect website URL')
+
+
+export const blogIdValidator = body('id')
+    .custom( (id:string) =>{
+    const blog = blogsRepo.getBlogById(id)
+    if (blog === false){
+        throw Error('incorrect id')}
+})*/
+const blogValidation = () => [nameValidator];
 exports.blogValidation = blogValidation;
