2be923046caf039532341ce0f97aa1f53b8c56b6
McRais <mcrais21@gmail.com>
2024-11-18 12:53:32 +0300

    Update posts-route.js

--- dist/routes/posts-route.js
+++ dist/routes/posts-route.js
@@ -16,30 +16,30 @@ const posts_repository_1 = require("../repo/posts-repository");
 const validator_posts_1 = require("../validators/validator-posts");
 exports.postsRoute = (0, express_1.Router)({});
 //get all posts
-exports.postsRoute.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const post = yield posts_repository_1.postsRepo.getAllPosts();
-    return res.send(post);
-})); //done
+exports.postsRoute.get('/', async(req, res), (Promise), {
+    const: post = await posts_repository_1.postsRepo.getAllPosts(),
+    return: res.send(post)
+}); //done
 //get post by id
-exports.postsRoute.get('/:id', (req, res) => {
-    const post = posts_repository_1.postsRepo.getPostById(req.params.id);
+exports.postsRoute.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const post = yield posts_repository_1.postsRepo.getPostById(req.params.id);
     if (post === false) {
         return res.sendStatus(404);
     }
     else {
         return res.send(post);
     }
-});
+}));
 //delete post by id, auth
-exports.postsRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) => {
-    const post = posts_repository_1.postsRepo.deletePost(req.params.id);
+exports.postsRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const post = yield posts_repository_1.postsRepo.deletePost(req.params.id);
     if (!post) {
         return res.sendStatus(404);
     }
     else {
         return res.sendStatus(204);
-    }
-});
+    } //not done
+}));
 //post a post, auth and validation
 exports.postsRoute.post("/", auth_middleware_1.authMiddleware, (0, validator_posts_1.postValidation)(), (req, res) => {
     const post = posts_repository_1.postsRepo.createNewPost(req.body.title, req.body.shortDescription, req.body.content, req.body.blogId);
