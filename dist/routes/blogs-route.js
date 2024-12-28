4ddfc8c83e75a93ece9db7f5e9131ca238e3c7f3
McRais <mcrais21@gmail.com>
2024-12-28 10:56:24 +0300

    Update blogs-route.js

--- dist/routes/blogs-route.js
+++ dist/routes/blogs-route.js
@@ -13,6 +13,7 @@ exports.blogsRoute = void 0;
 const express_1 = require("express");
 const auth_middleware_1 = require("../middlewares/auth-middleware");
 const blogs_repository_1 = require("../repo/blogs-repository");
+const validator_blogs_1 = require("../validators/validator-blogs");
 exports.blogsRoute = (0, express_1.Router)({});
 exports.blogsRoute.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const blogs = yield blogs_repository_1.blogsRepo.getAllBlogs();
@@ -36,7 +37,7 @@ exports.blogsRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) =
         return res.sendStatus(204);
     }
 }));
-exports.blogsRoute.post("/", auth_middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.blogsRoute.post("/", auth_middleware_1.authMiddleware, validator_blogs_1.blogParamsValidation, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const blogId = yield blogs_repository_1.blogsRepo.createNewBlog(req.body.name, req.body.description, req.body.websiteUrl);
     const blog = yield blogs_repository_1.blogsRepo.getBlogById(blogId);
     if (blog === false) {
