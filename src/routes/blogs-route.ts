2354192e8e25a7ae2356de4f8fc6bea496c2643b
McRais <mcrais21@gmail.com>
2024-12-28 10:56:21 +0300

    Update blogs-route.ts

--- src/routes/blogs-route.ts
+++ src/routes/blogs-route.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {blogsRepo} from "../repo/blogs-repository";
 import {OutputBlogType, RequestWithBody, RequestWithBodyAndParams} from "../models/types";
-import {blogIdValidation} from "../validators/validator-blogs";
+import {blogIdValidation, blogParamsValidation} from "../validators/validator-blogs";
 
 
 export const blogsRoute = Router({})
@@ -23,7 +23,7 @@ blogsRoute.delete('/:id',authMiddleware, async (req:Request, res:Response) =>{
     if (result === false){return res.sendStatus(404)} else {return res.sendStatus(204)}
 })
 
-blogsRoute.post("/",authMiddleware, async (req:RequestWithBody<{name:string, description:string, websiteUrl:string}>, res:Response) =>{
+blogsRoute.post("/",authMiddleware, blogParamsValidation, async (req:RequestWithBody<{name:string, description:string, websiteUrl:string}>, res:Response) =>{
     const blogId = await blogsRepo.createNewBlog(req.body.name, req.body.description, req.body.websiteUrl)
     const blog = await blogsRepo.getBlogById(blogId)
     if (blog === false){return res.sendStatus(404)} else {return res.status(201).send(blog)}
