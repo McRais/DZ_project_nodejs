2c33f892a0d9d065753e0c3f071cfee26b53860f
McRais <mcrais21@gmail.com>
2025-01-16 19:01:21 +0300

    Update blogs-repository.ts

--- src/repo/blogs-repository.ts
+++ src/repo/blogs-repository.ts
@@ -9,10 +9,21 @@ export class blogsRepo {
         return await blogsCollection.countDocuments({})
     }
 
-    static async getAllBlogs(pageNumber:number, pageSize:number, sortBy:string|null, sortDirection:string|null): Promise<OutputBlogType[]> {
+    static async getAllBlogs(searchNameTerm: string|null, pageNumber:number, pageSize:number, sortBy:string, sortDirection:string|null): Promise<OutputBlogType[]> {
         let field = "createdAt"
         if(sortBy!=null){field = sortBy}
         let blogs
+
+        if(searchNameTerm!=null){
+            const regexp = new RegExp(searchNameTerm, "i");
+            if(sortDirection=="asc"){
+                blogs = await blogsCollection.find({name:regexp}).sort({[field]:1}).skip((pageNumber-1)*pageSize).limit(pageSize).toArray()
+            } else {
+                blogs = await blogsCollection.find({name:regexp}).sort({[field]:-1}).skip((pageNumber-1)*pageSize).limit(pageSize).toArray()
+            }
+            return blogs.map(blogsMapper)
+        }
+
         if(sortDirection=="asc"){
             blogs = await blogsCollection.find({}).sort({[field]:1}).skip((pageNumber-1)*pageSize).limit(pageSize).toArray()
         } else {
