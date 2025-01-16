073dd451397e96f021590887753f2b4625f6fbd7
McRais <mcrais21@gmail.com>
2025-01-16 19:01:19 +0300

    Update blogs-route.ts

--- src/routes/blogs-route.ts
+++ src/routes/blogs-route.ts
@@ -11,13 +11,14 @@ import {postsRepo} from "../repo/posts-repository";
 export const blogsRoute = Router({})
 
 
-blogsRoute.get('/', async (req: RequestWithParams<{pageNumber:number, pageSize:number, sortBy:string|null, sortDirection:string|null}>, res: Response): Promise<Response<OutputBlogType[]>> => {
-    const blogs = await blogsRepo.getAllBlogs(req.params.pageNumber, req.params.pageSize, req.params.sortBy, req.params.sortDirection)
+blogsRoute.get('/', async (req: RequestWithParams<{searchNameTerm: string|null, pageNumber:number|null, pageSize:number|null, sortBy:string|null, sortDirection:string|null}>, res: Response): Promise<Response<OutputBlogType[]>> => {
+    const [searchNameTerm, pageNumber,pageSize,sortBy,sortDirection] = [req.params.searchNameTerm, req.params.pageNumber||1, req.params.pageSize||10, req.params.sortBy||"createdAt", req.params.sortDirection];
+    const blogs = await blogsRepo.getAllBlogs(searchNameTerm, pageNumber, pageSize, sortBy, sortDirection)
 
     return res.send({
-        "pagesCount": Math.ceil(blogs.length/req.params.pageSize),
-        "page": req.params.pageNumber,
-        "pageSize": req.params.pageSize,
+        "pagesCount": Math.ceil(blogs.length/pageSize),
+        "page": pageNumber,
+        "pageSize": pageSize,
         "totalCount": await blogsRepo.getCount(),
         "items": blogs
     })
