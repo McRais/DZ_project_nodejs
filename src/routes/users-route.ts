f7280ad673479fdb23fc1d426d853dcf4bb7afe7
McRais <mcrais21@gmail.com>
2025-02-12 15:16:49 +0300

    Create Users-route and repo

---
+++ src/routes/users-route.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+
+export const usersRoute = Router({});
+
+usersRoute.get('/', (req, res) => {})
+
+usersRoute.post('/', (req, res) => {})
+
+usersRoute.delete('/:id', (req, res) => {})
