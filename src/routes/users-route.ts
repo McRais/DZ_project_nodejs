0184350e577eaa9672d97a3272536f1a03b3a09a
McRais <mcrais21@gmail.com>
2025-02-13 18:11:11 +0300

    Update users-route.ts

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,8 +1,23 @@
-import {Router} from "express";
+import {Response, Router} from "express";
+import {OutputUserType, RequestWithQuery} from "../models/types";
+import {SortDirection} from "mongodb";
+import {usersRepo} from "../repo/users-repository";
 
 export const usersRoute = Router({});
 
-usersRoute.get('/', (req, res) => {})
+usersRoute.get('/', async (req: RequestWithQuery<{searchLoginTerm?: string,searchEmailTerm?: string, pageNumber?:number, pageSize?:number, sortBy?:string, sortDirection?:SortDirection}>, res: Response): Promise<Response<OutputUserType[]>> => {
+    const [searchLoginTerm,searchEmailTerm, pageNumber,pageSize,sortBy,sortDirection] = [req.query.searchLoginTerm,req.query.searchEmailTerm, Number(req.query.pageNumber||1), Number(req.query.pageSize||10), String(req.query.sortBy||"createdAt"), req.query.sortDirection as SortDirection||"desc"];
+    const users = await usersRepo.getAllUsers(searchLoginTerm, pageNumber, pageSize, sortBy, sortDirection)
+    const usersRepoCount = await usersRepo.getCount()
+
+    return res.send({
+        "pagesCount": Math.ceil(usersRepoCount/pageSize),
+        "page": pageNumber,
+        "pageSize": pageSize,
+        "totalCount": usersRepoCount,
+        "items": users
+    })
+}) //not done
 
 usersRoute.post('/', (req, res) => {})
 
