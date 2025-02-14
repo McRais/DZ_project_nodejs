9dfa84b634004d492a20d4ae1dd4ea1fb5fc1a1d
McRais <mcrais21@gmail.com>
2025-02-14 19:15:29 +0300

    Update blogs-repository.ts

--- src/repo/blogs-repository.ts
+++ src/repo/blogs-repository.ts
@@ -29,7 +29,7 @@ export class blogsRepo {
         if (!blog) {
             return false
         }
-        const blogArr = Array.of(blog)
+        const blogArr = Array.of(blog) //eugene please refactor this, there is a lot of crutches already. Sincerely, Eugene
         return blogArr.map(blogsMapper)[0]
     }
 
