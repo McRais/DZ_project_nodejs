76b38c447883a7bd00e523f7fba65585403b94e5
McRais <mcrais21@gmail.com>
2025-02-17 15:06:20 +0300

    Update users-route.ts

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,11 +1,12 @@
-import {Response, Router} from "express";
+import e, {Response, Router} from "express";
 import {OutputUserType, RequestWithBody, RequestWithParams, RequestWithQuery} from "../models/types";
 import {SortDirection} from "mongodb";
 import {usersRepo} from "../repo/users-repository";
+import {authMiddleware} from "../middlewares/auth-middleware";
 
 export const usersRoute = Router({});
 
-usersRoute.get('/', async (req: RequestWithQuery<{searchLoginTerm?: string,searchEmailTerm?: string, pageNumber?:number, pageSize?:number, sortBy?:string, sortDirection?:SortDirection}>, res: Response): Promise<Response<OutputUserType[]>> => {
+usersRoute.get('/',authMiddleware, async (req: RequestWithQuery<{searchLoginTerm?: string,searchEmailTerm?: string, pageNumber?:number, pageSize?:number, sortBy?:string, sortDirection?:SortDirection}>, res: Response): Promise<Response<OutputUserType[]>> => {
     const [searchLoginTerm,searchEmailTerm, pageNumber,pageSize,sortBy,sortDirection] = [req.query.searchLoginTerm,req.query.searchEmailTerm, Number(req.query.pageNumber||1), Number(req.query.pageSize||10), String(req.query.sortBy||"createdAt"), req.query.sortDirection as SortDirection||"desc"];
     const users = await usersRepo.getAllUsers(searchLoginTerm, searchEmailTerm, pageNumber, pageSize, sortBy, sortDirection)
     const usersRepoCount = await usersRepo.getCount()
@@ -19,7 +20,7 @@ usersRoute.get('/', async (req: RequestWithQuery<{searchLoginTerm?: string,searc
     })
 })
 
-usersRoute.post('/', async (req: RequestWithBody<{login:string, password:string, email:string}>, res) => {
+usersRoute.post('/', authMiddleware, async (req: RequestWithBody<{login:string, password:string, email:string}>, res:Response): Promise<Response<OutputUserType|400>> => {
 
     if(await usersRepo.checkUserLoginUniqueness(req.body.login)){
         return res.send(400).json({
@@ -41,13 +42,13 @@ usersRoute.post('/', async (req: RequestWithBody<{login:string, password:string,
             ]
         })
     }
-    
     const [login, password, email, createdAt] = [req.body.login, req.body.password, req.body.email, new Date]
     const user = await usersRepo.createUser(login, password, email, createdAt.toISOString())
     return res.status(201).send(user)
 })
 
-usersRoute.delete('/:id', async (req:RequestWithParams<{id:string}>, res) => {
-    const result = await usersRepo.deleteUser(req.params.id)    //redo later
-    return res.status(200).send({result})
+usersRoute.delete('/:id',authMiddleware, async (req:RequestWithParams<{id:string}>, res: Response): Promise<Response<204|404>> => {
+
+    const result = await usersRepo.deleteUser(req.params.id)   
+    return result? res.sendStatus(204):res.sendStatus(404)
 })
