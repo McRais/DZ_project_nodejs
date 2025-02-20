11a1c1f6b31fa025249f04ae252ff43b700c28b1
McRais <mcrais21@gmail.com>
2025-02-20 20:40:31 +0300

    fix validator for users

--- dist/validators/validator-users.js
+++ dist/validators/validator-users.js
@@ -10,11 +10,11 @@ const loginValidator = (0, express_validator_1.body)('login')
     min: 3,
     max: 100
 }).withMessage('incorrect login length')
-    .matches(/^[a-zA-Z0-9_-]*$/, 'incorrect login pattern');
-/*const emailValidator = body('email')
+    .matches('^[a-zA-Z0-9_-]*$').withMessage('incorrect login pattern');
+const emailValidator = (0, express_validator_1.body)('email')
     .isString().withMessage('website URL must be a string')
     .trim()
-    .matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$').withMessage('incorrect email address')*/
+    .matches('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$').withMessage('incorrect email address');
 const passwordValidator = (0, express_validator_1.body)('password')
     .isString().withMessage('password must be a string')
     .trim()
@@ -22,5 +22,5 @@ const passwordValidator = (0, express_validator_1.body)('password')
     min: 6,
     max: 20
 }).withMessage('incorrect password length');
-const userValidator = () => [loginValidator, /*emailValidator,*/ passwordValidator, validator_errors_catcher_1.validatorErrorsCatcher];
+const userValidator = () => [loginValidator, emailValidator, passwordValidator, validator_errors_catcher_1.validatorErrorsCatcher];
 exports.userValidator = userValidator;
