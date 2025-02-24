dfdfedeb6d8359b4de0b1694844250bbf02cea8f
McRais <mcrais21@gmail.com>
2025-02-24 14:13:41 +0300

    Update users-route.js

--- dist/routes/users-route.js
+++ dist/routes/users-route.js
@@ -13,6 +13,7 @@ exports.usersRoute = void 0;
 const express_1 = require("express");
 const users_repository_1 = require("../repo/users-repository");
 const auth_middleware_1 = require("../middlewares/auth-middleware");
+const validator_users_1 = require("../validators/validator-users");
 exports.usersRoute = (0, express_1.Router)({});
 exports.usersRoute.get('/', auth_middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const [searchLoginTerm, searchEmailTerm, pageNumber, pageSize, sortBy, sortDirection] = [req.query.searchLoginTerm, req.query.searchEmailTerm, Number(req.query.pageNumber || 1), Number(req.query.pageSize || 10), String(req.query.sortBy || "createdAt"), req.query.sortDirection || "desc"];
@@ -26,14 +27,11 @@ exports.usersRoute.get('/', auth_middleware_1.authMiddleware, (req, res) => __aw
         "items": users
     });
 }));
-/*usersRoute.post('/', authMiddleware, userValidator, async (req: RequestWithBody<{ login: string, password: string, email: string }>, res: Response): Promise<Response<OutputUserType|400>> => {
-    const checkEmailUniqueness = await usersRepo.checkUserEmailUniqueness(req.body.email)
-    if (!checkEmailUniqueness) {}
-
-    const [login, password, email, createdAt] = [req.body.login, req.body.password, req.body.email, new Date]
-    const user = await usersRepo.createUser(login, password, email, createdAt.toISOString())
-    return res.status(201).send(user)
-})*/
+exports.usersRoute.post('/', auth_middleware_1.authMiddleware, (0, validator_users_1.userValidator)(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const [login, password, email, createdAt] = [req.body.login, req.body.password, req.body.email, new Date];
+    const user = yield users_repository_1.usersRepo.createUser(login, password, email, createdAt.toISOString());
+    return res.status(201).send(user);
+}));
 exports.usersRoute.delete('/:id', auth_middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const result = yield users_repository_1.usersRepo.deleteUser(req.params.id);
     return result ? res.sendStatus(204) : res.sendStatus(404);
