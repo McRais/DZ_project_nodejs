dca71a5e34a3bdef4af5813fd800241a2516d17b
McRais <mcrais21@gmail.com>
2025-02-24 14:13:35 +0300

    Update validator-users.ts

--- src/validators/validator-users.ts
+++ src/validators/validator-users.ts
@@ -1,4 +1,6 @@
 import {body} from "express-validator";
+import {validatorErrorsCatcher} from "../middlewares/validator-errors-catcher";
+import {usersRepo} from "../repo/users-repository";
 
 const loginValidator = body('login')
     .isString().withMessage('login must be a string')
@@ -22,4 +24,18 @@ const passwordValidator = body('password')
         max: 20
     }).withMessage('incorrect password length')
 
-export const userValidator = () => [loginValidator, emailValidator, passwordValidator]
+const loginUniquenessValidator = body('login').custom(async(login) => {
+    const loginUniqueness =  await usersRepo.checkUserLoginUniqueness(login)
+    if (!loginUniqueness) {
+        throw new Error('login is not unique')
+    }
+})
+
+const emailUniquenessValidator = body('email').custom(async(email) => {
+    const emailUniqueness =  await usersRepo.checkUserEmailUniqueness(email)
+    if (!emailUniqueness) {
+        throw new Error('email is not unique')
+    }
+})
+
+export const userValidator = () => [loginValidator, emailValidator, passwordValidator,loginUniquenessValidator,emailUniquenessValidator, validatorErrorsCatcher]
