fac5b264a3a115bfa3fc192c6a4932248bcbb982
McRais <mcrais21@gmail.com>
2025-02-28 17:58:05 +0300

    Update dist

---
+++ dist/routes/comments-route.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commentsRoute = void 0;
+const express_1 = require("express");
+exports.commentsRoute = (0, express_1.Router)({});
+exports.commentsRoute.get('', (req, res) => { });
+exports.commentsRoute.put('', (req, res) => { });
+exports.commentsRoute.delete('', (req, res) => { });
