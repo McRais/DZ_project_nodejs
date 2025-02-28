2718e76b031003241647e0e42d047e711f40905c
McRais <mcrais21@gmail.com>
2025-02-28 17:57:32 +0300

    Create comments-route.ts

---
+++ src/routes/comments-route.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+
+export const commentsRoute = Router({})
+
+commentsRoute.get('', (req, res) => {})
+
+commentsRoute.put('', (req, res) => {})
+
+commentsRoute.delete('', (req, res) => {})
