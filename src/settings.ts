697554fa61918aa395149d427c6b542999738f31
McRais <mcrais21@gmail.com>
2025-02-28 17:57:52 +0300

    Create comments route

--- src/settings.ts
+++ src/settings.ts
@@ -5,6 +5,7 @@ import {testingRoute} from "./routes/testing-route";
 import bodyParser from "body-parser";
 import {usersRoute} from "./routes/users-route";
 import {loginRoute} from "./routes/login-route";
+import {commentsRoute} from "./routes/comments-route";
 
 export const app = express()
 app.use(express.json())
@@ -14,4 +15,5 @@ app.use('/blogs', blogsRoute)
 app.use('/posts', postsRoute)
 app.use('/users', usersRoute)
 app.use('/auth/login', loginRoute)
+app.use('/comments', commentsRoute)
 app.use('/testing/all-data', testingRoute)
