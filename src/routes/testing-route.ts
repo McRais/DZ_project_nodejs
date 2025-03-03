00e1e6e68c7bac7c72bd1c6b2ad05a499d5039e6
McRais <mcrais21@gmail.com>
2025-03-03 15:22:20 +0300

    Update testing-route.ts

--- src/routes/testing-route.ts
+++ src/routes/testing-route.ts
@@ -1,10 +1,11 @@
 import {Request, Response, Router} from "express";
-import {blogsCollection, postsCollection, usersCollection} from "../database/DB";
+import {blogsCollection, commentsCollection, postsCollection, usersCollection} from "../database/DB";
 
 export const testingRoute = Router({})
 testingRoute.delete("/", async (req: Request, res: Response) => {
     await blogsCollection.drop()
     await postsCollection.drop()
     await usersCollection.drop()
+    await commentsCollection.drop()
     return res.sendStatus(204)
 })
