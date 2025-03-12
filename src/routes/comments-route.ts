42c731a5cc75c531f072c24f4b1c4e78aabeee6f
McRais <mcrais21@gmail.com>
2025-03-12 16:15:14 +0300

    Update comments-route.ts

--- src/routes/comments-route.ts
+++ src/routes/comments-route.ts
@@ -2,8 +2,12 @@ import {Router} from "express";
 
 export const commentsRoute = Router({})
 
+
+//get comment by id
 commentsRoute.get('', (req, res) => {})
 
+//update comment by id
 commentsRoute.put('', (req, res) => {})
 
+//delete comment by id
 commentsRoute.delete('', (req, res) => {})
