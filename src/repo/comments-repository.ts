0c4802c753f45c1f6d0e900ac1431f846a245d6f
McRais <mcrais21@gmail.com>
2025-03-20 18:37:20 +0300

    Update getCommentsFromPost

--- src/repo/comments-repository.ts
+++ src/repo/comments-repository.ts
@@ -1,21 +1,21 @@
 import {SortDirection} from "mongodb";
-import {commentsCollection, postsCollection} from "../database/DB";
+import {commentsCollection} from "../database/DB";
+import {commentsMapper} from "../mappers/mappers";
 
 export class commentsRepo{
+
     static async getCommentsFromPostCount(postId: string):  Promise<number>{
-        return await commentsCollection.countDocuments({postId: postId}) //feels not right, check the types.ts
+        return await commentsCollection.countDocuments({postId: postId})
     }
 
     static async getCommentsFromPost(postId: string, pageNumber:number, pageSize:number, sortBy:string, sortDirection:SortDirection): Promise<any>{
-
-        //need to add link to types, probably array to every post containing commentsId's
-        const posts = await commentsCollection
-            .find({commentatorInfo:{postId:postId}})
+        const comments = await commentsCollection
+            .find({postId: postId})
             .sort(sortBy, sortDirection)
             .limit(pageSize)
             .skip((pageNumber - 1) * pageSize)
             .toArray()
-
+        return comments.map(commentsMapper)
     }
 
 }
