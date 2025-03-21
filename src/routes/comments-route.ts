0d657c1afcee6165cf5cb965a6be615a9c788982
McRais <mcrais21@gmail.com>
2025-03-21 19:55:29 +0300

    Update comments-route.ts

--- src/routes/comments-route.ts
+++ src/routes/comments-route.ts
@@ -1,11 +1,14 @@
-import {Router} from "express";
+import {Router, Response} from "express";
+import {RequestWithParams} from "../models/types";
+import {commentsRepo} from "../repo/comments-repository";
 
 export const commentsRoute = Router({})
 
 
 //get comment by id
-commentsRoute.get('', (req, res) => {
-
+commentsRoute.get('/:id', async (req: RequestWithParams<{id:string}>, res:Response) : Promise<any> => {
+    const comment = await commentsRepo.getCommentById(req.params.id);
+    return res.status(200).send(comment);
 })
 
 //update comment by id
