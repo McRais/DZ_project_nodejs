f5baa813b3a6a0d18d6e34c48d40d848d1f24fa3
McRais <mcrais21@gmail.com>
2025-03-21 19:55:35 +0300

    Update comments-repository.js

--- dist/repo/comments-repository.js
+++ dist/repo/comments-repository.js
@@ -10,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.commentsRepo = void 0;
+const mongodb_1 = require("mongodb");
 const DB_1 = require("../database/DB");
 const mappers_1 = require("../mappers/mappers");
 class commentsRepo {
@@ -27,6 +28,16 @@ class commentsRepo {
                 .skip((pageNumber - 1) * pageSize)
                 .toArray();
             return comments.map(mappers_1.commentsMapper);
+        });
+    }
+    static getCommentById(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const comment = yield DB_1.commentsCollection.findOne({ _id: new mongodb_1.ObjectId(id) });
+            if (!comment) {
+                return false;
+            }
+            const commentArr = Array.of(comment);
+            return commentArr.map(mappers_1.commentsMapper)[0];
         });
     }
 }
