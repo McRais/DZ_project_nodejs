90943b4843efb98cc97de1be869550dd1328a796
McRais <mcrais21@gmail.com>
2025-04-01 09:29:24 +0300

    Update basic-auth-middleware.ts

--- src/middlewares/basic-auth-middleware.ts
+++ src/middlewares/basic-auth-middleware.ts
@@ -1,6 +1,6 @@
 import {NextFunction, Request, Response} from "express";
 
-export const BasicAccessAuthMiddleware = (req: Request, res: Response, next:NextFunction) => {
+export const BasicAuthMiddleware = (req: Request, res: Response, next:NextFunction) => {
     if (req.headers['authorization'] !== "Basic YWRtaW46cXdlcnR5"){
         res.sendStatus(401)
         return
