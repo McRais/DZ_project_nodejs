95a58852bc99630697f388d2360ddb573e8656af
McRais <mcrais21@gmail.com>
2025-04-14 19:37:42 +0300

    Update auth-with-bearer-token-middleware.ts

--- src/middlewares/auth-with-bearer-token-middleware.ts
+++ src/middlewares/auth-with-bearer-token-middleware.ts
@@ -1,6 +1,5 @@
 import {NextFunction, Request, Response} from "express";
 
 export const AuthWithBearerTokenMiddleware = (req: Request, res: Response, next:NextFunction) => {
-
     return next()
 }
