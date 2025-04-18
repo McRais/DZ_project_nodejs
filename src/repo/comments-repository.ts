13ae9d5f7c1c56a5c0f34ad25b9d3580fe0605b0
McRais <mcrais21@gmail.com>
2025-04-18 18:23:54 +0300

    Update comments-repository.ts

--- src/repo/comments-repository.ts
+++ src/repo/comments-repository.ts
@@ -1,6 +1,6 @@
 import {ObjectId, SortDirection} from "mongodb";
 import {commentsCollection} from "../database/DB";
-import {commentsMapper} from "../mappers/mappers";
+import {commentsOutputMapper} from "../mappers/mappers";
 import {OutputCommentType} from "../models/types";
 
 export class commentsRepo{
@@ -16,14 +16,14 @@ export class commentsRepo{
             .limit(pageSize)
             .skip((pageNumber - 1) * pageSize)
             .toArray()
-        return comments.map(commentsMapper)
+        return comments.map(commentsOutputMapper)
     }
 
     static async getCommentById(id:string): Promise<OutputCommentType|false> {
         const comment = await commentsCollection.findOne({_id: new ObjectId(id)})
         if (!comment) {return false}
         const commentArr = Array.of(comment)
-        return commentArr.map(commentsMapper)[0]
+        return commentArr.map(commentsOutputMapper)[0]
     }
 
     static async createComment(content:string, postId:string): Promise<string|false> {
