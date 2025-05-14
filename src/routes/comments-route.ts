a0ecf226e1fd60b09034a3ab95418aca94d950f7
McRais <mcrais21@gmail.com>
2025-05-14 17:49:32 +0300

    Update comments-route.ts

--- src/routes/comments-route.ts
+++ src/routes/comments-route.ts
@@ -1,7 +1,7 @@
 import {Router, Response} from "express";
 import {RequestWithParams} from "../models/types";
 import {commentsRepo} from "../repo/comments-repository";
-import {AuthBearerMiddleware} from "../middlewares/auth-bearer-middleware";
+import {BearerAuthMiddleware} from "../middlewares/bearer-auth-middleware";
 
 export const commentsRoute = Router({})
 
@@ -12,7 +12,7 @@ commentsRoute.get('/:id', async (req: RequestWithParams<{id:string}>, res:Respon
 })
 
 //update comment by id
-commentsRoute.put('', AuthBearerMiddleware, (req, res) => {})
+commentsRoute.put('', BearerAuthMiddleware, (req, res) => {})
 
 //delete comment by id
-commentsRoute.delete('', AuthBearerMiddleware, (req, res) => {})
+commentsRoute.delete('', BearerAuthMiddleware, (req, res) => {})
