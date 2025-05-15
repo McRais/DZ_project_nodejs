950fd2724bdb229ef0894d0f0216f43d0921a5b4
McRais <mcrais21@gmail.com>
2025-05-15 09:58:40 +0300

    Create auth-bearer-middleware.js

---
+++ dist/middlewares/auth-bearer-middleware.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthBearerMiddleware = void 0;
+const AuthBearerMiddleware = (req, res, next) => {
+    return next();
+};
+exports.AuthBearerMiddleware = AuthBearerMiddleware;
