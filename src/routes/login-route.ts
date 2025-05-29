572fe1dd1abedebed538c59c1e9c1475ab141184
McRais <mcrais21@gmail.com>
2025-05-29 16:34:51 +0300

    Update login-route.ts

--- src/routes/login-route.ts
+++ src/routes/login-route.ts
@@ -1,14 +1,16 @@
 import {Response, Router} from "express";
-import {RequestWithBody} from "../models/types";
+import {MyInfoType, RequestWithBody} from "../models/types";
 import {usersRepo} from "../repo/users-repository";
 
 export const loginRoute = Router({});
 
 //login user, needs update and validator for input values
-loginRoute.post('/', async (req: RequestWithBody<{loginOrEmail: string, password:string}>, res:Response): Promise<Response<200|401|400>> => {
+loginRoute.post('/login', async (req: RequestWithBody<{loginOrEmail: string, password:string}>, res:Response): Promise<Response<200|401|400>> => {
     const jwtOfUser = await usersRepo.loginUser(req.body.loginOrEmail, req.body.password);
     if (jwtOfUser) {
         return res.status(200).send(jwtOfUser);
     }
     return res.sendStatus(401)
 })
+
+//loginRoute.get('/me', async (req: RequestWithBody<{me: string}>, res:Response): Promise<MyInfoType|401> => {})
