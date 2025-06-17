46c10ce4d477aef4e53cf2ea421e34a339ed382b
McRais <mcrais21@gmail.com>
2025-06-17 11:31:35 +0300

    Update dist

--- dist/routes/comments-route.js
+++ dist/routes/comments-route.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.commentsRoute = void 0;
 const express_1 = require("express");
 const comments_repository_1 = require("../repo/comments-repository");
-const bearer_auth_middleware_1 = require("../middlewares/bearer-auth-middleware");
+const auth_bearer_middleware_1 = require("../middlewares/auth-bearer-middleware");
 exports.commentsRoute = (0, express_1.Router)({});
 //get comment by id
 exports.commentsRoute.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -20,7 +20,7 @@ exports.commentsRoute.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0
     return res.status(200).send(comment);
 }));
 //update comment by id
-exports.commentsRoute.put('', bearer_auth_middleware_1.BearerAuthMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.commentsRoute.put('', auth_bearer_middleware_1.AuthBearerMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const comment = yield comments_repository_1.commentsRepo.updateComment(req.params.id, req.body.content);
     if (!comment) {
         return res.sendStatus(404);
@@ -30,5 +30,8 @@ exports.commentsRoute.put('', bearer_auth_middleware_1.BearerAuthMiddleware, (re
     }
 }));
 //delete comment by id
-exports.commentsRoute.delete('', bearer_auth_middleware_1.BearerAuthMiddleware, (req, res) => {
-});
+/*commentsRoute.delete('', AuthBearerMiddleware, async (req:RequestWithParams<{id:string}>, res:Response):Promise<204|404|403> => {
+    const result = await usersRepo.deleteUser(req.params.id)
+    return
+
+})*/ 
