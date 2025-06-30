9def3ebfc90ecd48296f28eb23eca4fea517f57c
McRais <mcrais21@gmail.com>
2025-06-30 15:29:36 +0300

    Comment faulty lines for testing

--- src/middlewares/auth-bearer-middleware.ts
+++ src/middlewares/auth-bearer-middleware.ts
@@ -1,5 +1,17 @@
 import {NextFunction, Request, Response} from "express";
+import {usersRepo} from "../repo/users-repository";
+import {jwtService} from "../services/jwt-service";
 
 export const AuthBearerMiddleware = (req: Request, res: Response, next:NextFunction) => {
-    return next()
+   /* if (!req.headers.authorization) {return res.sendStatus(401)}
+
+    const token = req.headers.authorization.split(' ')[1];
+    const userId = await jwtService.getUserIdFromToken(token);
+
+    if (userId){
+        req.userId=await usersRepo.getUser(userId)
+        return next()
+    }
+
+    return res.sendStatus(401)*/
 }
--- src/services/jwt-service.ts
+++ src/services/jwt-service.ts
@@ -1,10 +1,18 @@
 import {OutputUsersType} from "../models/types";
 import jwt from "jsonwebtoken";
+import {ObjectId} from "mongodb";
 
 
 export const jwtService = {
      async createJwt(user: OutputUsersType) {
-        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET||"123", {expiresIn: "10m"});
+        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET||'123', {expiresIn: "1h"});
         return {accessToken: token};
-    }
+    },
+    /*async getUserIdFromToken(token:string){
+         try {
+             const result = jwt.verify(token, process.env.JWT_SECRET || '123');
+             return result.userId;
+         }
+         catch (error) {return null}
+    }*/
 }
