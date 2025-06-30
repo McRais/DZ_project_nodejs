73d3e0b77f674c62aa48fc8cb798de8d11454b46
McRais <mcrais21@gmail.com>
2025-06-30 09:57:49 +0300

    Update login

--- src/routes/login-route.ts
+++ src/routes/login-route.ts
@@ -5,13 +5,11 @@ import {usersRepo} from "../repo/users-repository";
 export const loginRoute = Router({});
 
 
-//login user, needs update and validator for input values
-loginRoute.post('/login', async (req: RequestWithBody<{loginOrEmail: string, password:string}>, res:Response): Promise<Response<200|401|400>> => {
-
-    const login = usersRepo.loginUser(req.body.loginOrEmail, req.body.password);
+//login user, needs validator for input values
+loginRoute.post('/login', async (req: RequestWithBody<{loginOrEmail: string, password:string}>, res:Response): Promise<Response<204|401|400>> => {
+    const login = await usersRepo.loginUser(req.body.loginOrEmail, req.body.password);
     if(!login){return res.sendStatus(401)}
-    return res.sendStatus(200)
-
+    return res.sendStatus(204)
 })
 
 //loginRoute.get('/me', async (req: RequestWithBody<{me: string}>, res:Response): Promise<MyInfoType|401> => {})
