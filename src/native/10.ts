6a9e5eaa0d82a12c8ec5764b0aa3d3da9d0c04b0
McRais <mcrais21@gmail.com>
2025-07-21 20:31:34 +0300

    Create 10.ts

---
+++ src/native/10.ts
@@ -0,0 +1,50 @@
+/*
+//this
+//ссылка на объект
+
+console.log(this) //выдаст window
+
+const a = () => {
+    console.log(this)
+}
+a() // window ибо у стрелочной функции нет this
+
+function b() {
+    const a = () => {
+        console.log(this)
+    }
+    a()
+}
+b() //window, если сделать "use strict" будет undefined
+*/
+
+const bmw = {
+    brand: "bmw",
+    speed: 200,
+    showMaxSpeed() {
+        console.log(this.speed);
+    },
+};
+const scooter = {
+    speed: 60,
+};
+const kia = {
+    speed: 150,
+};
+bmw.showMaxSpeed.call(scooter);
+bmw.showMaxSpeed.apply(scooter);
+
+/*function foo(){
+    const x = 10
+
+    return {
+        X:10,
+        bar: () => {
+            console.log(this.x)
+        },
+        baz: function (){
+            console.log(this.x)
+        }
+
+    }
+}*/
