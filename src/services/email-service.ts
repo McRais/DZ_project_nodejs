1d7f37276f740acca24f3a706cf09a9d657ca2d4
McRais <mcrais21@gmail.com>
2025-07-24 09:56:52 +0300

    Update email-service.ts

--- src/services/email-service.ts
+++ src/services/email-service.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export class EmailService {
     static async RegisterUser(login: string, email: string, password: string): Promise<any> {
-        let email = await nodemailer.createTransport({
+        let mail = await nodemailer.createTransport({
 
         })
     }
