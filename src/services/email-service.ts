c1e05149e24cd696170f0de0dcf80337381f9f4f
McRais <mcrais21@gmail.com>
2025-07-28 15:35:42 +0300

    Update email-service.ts

--- src/services/email-service.ts
+++ src/services/email-service.ts
@@ -2,8 +2,19 @@ import nodemailer from 'nodemailer';
 
 export class EmailService {
     static async RegisterUser(login: string, email: string, password: string): Promise<any> {
-        let mail = await nodemailer.createTransport({
-
+        let transport = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            },
+        })
+        const mail = await transport.sendMail({
+            /*from: '"Example Team" <team@example.com>', // sender address
+            to: "alice@example.com, bob@example.com", // list of receivers
+            subject: "Hello", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body*/
         })
     }
 }
